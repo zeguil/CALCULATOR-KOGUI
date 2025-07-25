71d97b0f66d37eef280623fba863ffa0da43747a
Jose <zeguilhermelins@hotmail.com>
2025-07-25 11:54:03 -0400

    user views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+# core/views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import Operacao
+import operator
 
-# Create your views here.
+def registrar_usuario(request):
+    """view para registrar um novo usuário."""
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('calculadora')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/registrar.html', {'form': form})
+
+def login_usuario(request):
+    """view para login do usuário."""
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('calculadora')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+def logout_usuario(request):
+    """view para logout."""
+    logout(request)
+    return redirect('login')
+
